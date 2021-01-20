35ae235f701f9187f8dda13b65947621407e8f6a
samxpp <n9hopy00@students.oamk.fi>
2021-01-20 09:26:32 +0200

    First release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [height,setHeight] = useState(0);
+  const [weight,setWeight] = useState(0);
+  const [bmi,setBmi] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const result = weight / (height * height);
+    setBmi(result);
+    // also setBmiweight / (height * height);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{marginTop: 30}}>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <h2>Calculatin body mass index</h2>
+        <label>Height</label>
+        <input type="number" value={height} onChange={e => setHeight(e.target.value)} />
+      </div>
+      <div>
+      <label>Weight</label>
+        <input type="number" value={weight} onChange={e => setWeight(e.target.value)} />
+      </div>
+      <div className="form-group">
+        <label>Your BMI:</label>
+        <output className="form-control">{bmi}</output>
+      </div>
+      <button className="btn btn-primary">Calculate</button>
+    </form>
     </div>
   );
 }
